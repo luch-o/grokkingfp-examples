eba9e86b2de7f1687da6e234caa5fc639d264fb6
Luis Rodriguez <luiscarrodba97@gmail.com>
2026-03-05 20:13:10 -0500

    feat: add functional data model coffee break

---
+++ src/exercises/scala/ch07_coffee_break_functional_data_design.scala
@@ -0,0 +1,75 @@
+object model {
+    case class User(name: String)
+
+    case class Artist(name: String)
+
+    enum MusicGenre {
+        case HeavyMetal
+        case Rock
+        case Salsa
+        case Funk
+        case House
+    }
+
+    case class Song(name: String, artist: Artist)
+
+    enum PlaylistKind {
+        case Curated(curator: User)
+        case ArtistBased(artist: Artist)
+        case GenreBased(genres: List[MusicGenre])
+    }
+
+    case class Playlist(name: String, kind: PlaylistKind, songs: List[Song])
+}
+
+import model._
+import MusicGenre._
+import PlaylistKind._
+
+
+val ff = Artist("Foo Fighters")
+val thisIsFF = Playlist(
+    name="This is foo fighters", 
+    kind=ArtistBased(artist=ff), 
+    songs=List(
+        Song("Breakout", artist=ff),
+        Song("Learn to fly", artist=ff)
+    )
+)
+
+val deepFocus = Playlist(
+    name="Deep Focus",
+    kind=GenreBased(genres=List(
+        Funk, 
+        House
+    )),
+    songs=List(
+        Song("One More Time", artist=Artist("Daft Punk")),
+        Song("Hey Boye Hey Girl", artist=Artist("The Chemical Brothers"))
+    )
+)
+
+val myPlaylist = Playlist(
+    name="Luis Playlist",
+    kind=Curated(curator=User("Luis")),
+    songs=List.empty
+)
+
+def gatherSongs(playlists: List[Playlist], artist: Artist, genre: MusicGenre): List[Song] = {
+    // songs performed by the artist from user based playlist
+    // plus all songs from artist based playlists
+    // plus all songs from genre-based playlist
+    playlists.foldLeft(List.empty[Song])((songs, playlist) =>
+        val matchingSongs = playlist.kind match {
+            case Curated(curator) => 
+                playlist.songs.filter(_.artist == artist)
+            case ArtistBased(playlistArtist) => 
+                if (playlistArtist == artist) playlist.songs else List.empty
+            case GenreBased(genres) =>  
+                if (genres.contains(genre)) playlist.songs else List.empty
+        }
+        songs.appendedAll(matchingSongs)
+    )
+}
+
+gatherSongs(List(thisIsFF, deepFocus, myPlaylist), ff, Salsa)
